b6d6bc82a5a1e9af03fdedc8b66e918b90a7949a
ByteZ1337 <42820438+ByteZ1337@users.noreply.github.com>
2026-01-22 18:03:39 +0100

    chore: remove manual build scripts

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-TARGET=${1:-linux-cachyos-latest-x86-64-v3}
-docker build --file ./scripts/Dockerfile -t cachyos-builder .
-docker run --rm -it --privileged --env-file .env -e TARGET="$TARGET" cachyos-builder
